c3b53d00a2236889a6b90a3c51a821163b7ea6bf
gangh0824 <gangh0824@gmail.com>
2026-09-20 15:45:45 +0900

    professorComment DTO validation 수정

--- src/main/java/com/inu/jeongbobada/domain/professorComment/dto/ProfessorCommentUpdateRequestDto.java
+++ src/main/java/com/inu/jeongbobada/domain/professorComment/dto/ProfessorCommentUpdateRequestDto.java
@@ -1,6 +1,8 @@
 package com.inu.jeongbobada.domain.professorComment.dto;
 
 import com.inu.jeongbobada.domain.professorComment.entity.ProfessorCommentAnonymity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -8,13 +10,15 @@ import lombok.Getter;
 @Getter
 public class ProfessorCommentUpdateRequestDto {
     @NotNull
-    private int professorCommentRate;
+    @Min(1)
+    @Max(5)
+    private Integer professorCommentRate;
     @NotBlank
     private String professorCommentDetail;
     @NotNull
     private ProfessorCommentAnonymity professorCommentAnonymity;
 
-    public ProfessorCommentUpdateRequestDto(int professorCommentRate, String professorCommentDetail, ProfessorCommentAnonymity professorCommentAnonymity) {
+    public ProfessorCommentUpdateRequestDto(Integer professorCommentRate, String professorCommentDetail, ProfessorCommentAnonymity professorCommentAnonymity) {
         this.professorCommentRate = professorCommentRate;
         this.professorCommentDetail = professorCommentDetail;
         this.professorCommentAnonymity = professorCommentAnonymity;
